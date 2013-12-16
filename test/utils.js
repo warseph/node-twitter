7c483b3847b418aa31196b90d661b551c8c5220b
Desmond Morris <hi@desmondmorris.com>
2013-12-15 20:32:38 -0500

    Adds test coverage for the escape wrapper function

--- test/utils.js
+++ test/utils.js
@@ -1,8 +1,9 @@
+/*global escape: true */
 "use strict";
 
 var utils = require('../lib/utils');
 
-global.describe('Merge helper', function () {
+global.describe('Tests merge helper', function () {
     global.it('object should have all properties', function () {
         var objA = {'a': 'A', 'b': 'B'},
             objB = {'c': 'C', 'd': 'D'},
@@ -13,4 +14,13 @@ global.describe('Merge helper', function () {
         objC.should.have.property('d', 'D');
     });
 });
+
+global.describe('Tests escape wrapper', function () {
+    global.it('escape wrapper should return escape()', function () {
+        var str = 'test string';
+
+        utils.escape(str).should.equal(escape(str));
+    });
+});
+
 
